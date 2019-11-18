5f005c2808432074a8afe92dae15772da478df7d
Jeremiidesu <jerem.auz@gmail.com>
2019-11-18 13:17:23 +0100

    Ajouter classes de base et idée pour CUI

---
+++ src/reversi/cui/Controleur.java
@@ -0,0 +1,11 @@
+package reversi.cui;
+
+class Controleur
+{
+    Controleur()
+    {
+        // Essayer de reprendre la mentalité Frames
+        // Aka, chaque classe dispose de sa propre boucle, peut-être outsourcer les SOP
+        // pour pouvoir naviguer plus facilement "de scène en scène"
+    }
+}
---
+++ src/reversi/cui/Main.java
@@ -0,0 +1,9 @@
+package reversi.cui;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        new Controleur();
+    }
+}
